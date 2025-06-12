bc2642a8dc9640386f79e512435de2face914a20
AngelR <ing.angelrinformatica@gmail.com>
2025-06-12 12:49:48 -0400

    Restriccion de la sesion para el enrrutamiento en funcionamiento

--- PMBackend/frontend/paginas/js/script.js
+++ PMBackend/frontend/paginas/js/script.js
@@ -1,18 +1,36 @@
-// Al cargar la página principal
-if (localStorage.getItem('isLoggedIn') !== 'true') {
-  window.location.href = '/PMBackend/frontend/index.html'; // Redirige al login si no está autenticado
+document.addEventListener('DOMContentLoaded', function() {
+  // Verifica si el usuario está autenticado
+  if (localStorage.getItem('isLoggedIn') !== 'true') {
+    window.location.href = '/PMBackend/frontend/index.html'; // Redirige al login si no está autenticado
+  } else {
+    // Si está autenticado, muestra el nombre del cliente
+    const nombreCliente = localStorage.getItem('nombre_cliente');
+    const welcomeMessage = document.querySelector('.welcome-message');
+    if (welcomeMessage) {
+      welcomeMessage.textContent = `Bienvenido, ${nombreCliente}`;
+    }
+    const labelNombre = document.querySelector('.client-name');
+    if (labelNombre) {
+      labelNombre.textContent = `Bienvenido de nuevo ${nombreCliente}!`;
+    }
+  }
+  const btnLogout = document.querySelector('.btn-logout');
+  if (btnLogout) {
+    btnLogout.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log('SE EJECUTO ESA MIELDA');
+    // Limpia el almacenamiento local y redirige al login
+    localStorage.removeItem('id_cliente');
+    localStorage.removeItem('nombre_cliente');
+    localStorage.removeItem('correo_cliente');
+    localStorage.removeItem('isLoggedIn');
+    window.location.href = '/PMBackend/frontend/index.html'; // Redirige al login
+  });
 }
+});
+// Al cargar la página principal
+//if (localStorage.getItem('isLoggedIn') !== 'true') {
+//  window.location.href = '/PMBackend/frontend/index.html'; // Redirige al login si no está autenticado
+//}
 
 // Manejo del evento para cerrar sesión
-//const btnLogout = document.querySelector('.btn-logout');
-//if (btnLogout) {
-//  btnLogout.addEventListener('click', function(e) {
-//    e.preventDefault();
-//    // Limpia el almacenamiento local y redirige al login
-//    localStorage.removeItem('id_cliente');
-//    localStorage.removeItem('nombre_cliente');
-//    localStorage.removeItem('correo_cliente');
-//    localStorage.removeItem('isLoggedIn');
-//    window.location.href = 'login.html';
-//  });
-//}
